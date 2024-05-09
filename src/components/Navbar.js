05fa0b7faca9abb4967f3bd3acbce9b03e75902c
Yunus2006 <115533966+Yunus2006@users.noreply.github.com>
2024-05-09 15:58:49 +0200

    FKNFUNKA

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='leftSide'></div>
+        <div className='rightSide'></div>
+    </div>
+  )
+}
+
+export default Navbar
